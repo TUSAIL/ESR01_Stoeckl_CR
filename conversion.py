51a1c762c74a6625aeadb80dda508a029d734cde
gestoe <gero.stoeckl@gmx.net>
2023-05-17 08:55:35 +0200

    added documentation

--- conversion.py
+++ conversion.py
@@ -1,2 +1,27 @@
 def celsius_to_kelvin(celsius):
+    """
+    Function to return Kelvin degrees from Celsius input
+    """
     return celsius + 273.15
+
+def celsius_to_fahrenheit(celsius):
+    """
+    Function to return fahrenheit from Celsius input
+    
+    
+    """
+    return celsius *9/5 + 32
+
+
+def mean_temperature(data):
+    """
+    Get the mean temperature
+    
+    Arg:
+        data(pandas.DataFrame): A pandas dataframe with air temperature measurements.
+
+    Returns:
+        The mean air temperature (float)
+    """
+    temperatures = data['Air temperature (degC)']
+    return sum(temperatures)/len(temperatures)
